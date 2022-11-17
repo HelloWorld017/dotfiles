1daa8d961098a9801a22352f9d31f6d8b5455ba5
nenw <khi@nenw.dev>
2022-11-17 10:36:09 +0900

    fix: use nvim coc pum

--- modules/cli/nvim/default.nix
+++ modules/cli/nvim/default.nix
@@ -5,6 +5,16 @@
 		home.programs.neovim = {
 			enable = true;
 			extraConfig = (builtins.readFile ./init.nvim);
+			coc = {
+				enable = true;
+				settings = {
+					"eslint" = {
+						"autoFixOnSave" = true;
+					};
+					"suggest.noselect" = true;
+					"suggest.enablePreselect" = false;
+				};
+			};
 		};
 
 		env.EDITOR = "nvim";
