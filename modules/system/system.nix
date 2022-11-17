53f293f23e07b40b7b660bd16ff5b0b3f01d1b45
nenw <khi@nenw.dev>
2022-11-17 10:36:24 +0900

    fix: update key repeat config

--- modules/system/system.nix
+++ modules/system/system.nix
@@ -2,8 +2,8 @@
 {
 	config = {
 		system.defaults.NSGlobalDomain.AppleShowAllFiles = true;
-		system.defaults.NSGlobalDomain.InitialKeyRepeat = 40;
-		system.defaults.NSGlobalDomain.KeyRepeat = 3;
+		system.defaults.NSGlobalDomain.InitialKeyRepeat = 25;
+		system.defaults.NSGlobalDomain.KeyRepeat = 2;
 		system.defaults.trackpad.Clicking = true;
 		system.defaults.dock.autohide = true;
 	};
