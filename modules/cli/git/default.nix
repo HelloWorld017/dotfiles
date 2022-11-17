47b49728e98406d08ee25a6553ea8435cce7f59e
nenw <khi@nenw.dev>
2022-11-17 10:35:46 +0900

    fix: disable rs-git-fsmonitor

--- modules/cli/git/default.nix
+++ modules/cli/git/default.nix
@@ -10,11 +10,11 @@
 				alias.cpmps = "!f() { git checkout \"$1\" && git pull --rebase && git merge - && git push && git checkout -; }; f";
 				alias.bundlebranch = "!f() { git bundle create \"$2\" \"$1\" ^$(git merge-base \"$1\" \"master\"); }; f";
 				alias.l = "!sh -c 'PAGER=\"less -FRXK +/HEAD\" git log --oneline --graph --decorate'";
-				core.fsmonitor = "rs-git-fsmonitor";
 				pager.diff = "delta --plus-style 'syntax #205820'";
 				pull.ff = "only";
 				push.autoSetupRemote = "true";
 				rerere.enabled = "true";
+				# core.fsmonitor = "rs-git-fsmonitor";
 			};
 		};
 
