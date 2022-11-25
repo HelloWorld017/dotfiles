d601b8d104e48bc3b880eb768ac6c8d99d12ecd0
nenw <khi@nenw.dev>
2022-11-25 15:07:14 +0900

    feat: update neovim configs

--- modules/cli/nvim/default.nix
+++ modules/cli/nvim/default.nix
@@ -2,6 +2,7 @@
 
 {
 	config = {
+		home.packages = with pkgs; [ nil ];
 		home.programs.neovim = {
 			enable = true;
 			extraConfig = (builtins.readFile ./init.nvim);
@@ -13,6 +14,41 @@
 					};
 					"suggest.noselect" = true;
 					"suggest.enablePreselect" = false;
+					"suggest.completionItemKindLabels" = {
+						"text" = "  (text)";
+						"method" = " (method)";
+						"function" = " (function)";
+						"constructor" = " (constructor)";
+						"field" = " (field)";
+						"variable" = " (variable)";
+						"class" = " (class)";
+						"interface" = " (interface)";
+						"module" = " (module)";
+						"property" = " (property)";
+						"unit" = " (unit)";
+						"value" = " (value)";
+						"enum" = " (enum)";
+						"enumMember" = " (enum member)";
+						"keyword" = " (keyword)";
+						"snippet" = " (snippet)";
+						"color" = " (color)";
+						"file" = " (file)";
+						"reference" = " (reference)";
+						"folder" = " (folder)";
+						"constant" = " (constant)";
+						"struct" = " (struct)";
+						"event" = " (event)";
+						"operator" = " (operator)";
+						"typeParameter" = " (type parameter)";
+						"default" = " (default)";
+					};
+					"languageserver" = {
+						"nix" = {
+							"command" = "nil";
+							"filetypes" = ["nix"];
+							"rootPatterns" = ["flake.nix"];
+						};
+					};
 				};
 			};
 		};
