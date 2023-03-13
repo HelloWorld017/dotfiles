ede21ea39328f39636fa7c4c3dccf28609d9168f
nenw <khi@nenw.dev>
2023-03-13 18:04:59 +0900

    feat(git): make remote as default checkout remote

--- modules/cli/git/default.nix
+++ modules/cli/git/default.nix
@@ -11,6 +11,7 @@
 				alias.bundlebranch = "!f() { git bundle create \"$2\" \"$1\" ^$(git merge-base \"$1\" \"master\"); }; f";
 				alias.bundlestash = "!f() { git tag git-bundlestash-tag \"stash@{$1}\"; git bundle create \"$2\" git-bundlestash-tag ^master; git tag -d git-bundlestash-tag; }; f";
 				alias.l = "!sh -c 'PAGER=\"less -FRXK +/HEAD\" git log --oneline --graph --decorate'";
+				checkout.defaultRemote = "origin";
 				pager.diff = "delta --plus-style 'syntax #205820'";
 				pull.ff = "only";
 				push.autoSetupRemote = "true";
