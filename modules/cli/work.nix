85b0040f6520649c98998f9a750c3c98fb5e0a51
nenw <khi@nenw.dev>
2023-05-15 14:38:05 +0900

    feat: add work alias, cli apps

--- modules/cli/work.nix
+++ modules/cli/work.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
+	home.packages = with pkgs; [
+		_1password
+	];
+
 	home.programs.zsh = {
 		shellAliases = {
 			"fzclones" = "echo \"$HOME/workspace/ridi-clones/$(ls \"$HOME/workspace/ridi-clones\" | fzf)\"";
@@ -8,5 +12,18 @@
 			"cdc" = "cd `fzclones`";
 			"unsafe-chrome" = "open -n -a /Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --args --user-data-dir=\"$HOME/workspace/drafts/unsafe-chrome-userdata\" --disable-web-security";
 		};
+
+		initExtra = ''
+			clone_ridi () {
+				cd ~/workspace/ridi-clones
+				git clone ~/workspace/ridi-original $1
+
+				cd $1
+				git remote rename origin root
+				git remote add origin ssh://git@github.com/ridi/ridi
+				git checkout master
+				git config --list | grep remote=root | cut -d'=' -f1 | xargs -n 1 git config --unset
+			}
+		'';
 	};
 }
